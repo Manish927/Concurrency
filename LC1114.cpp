42f8f0c5d3ff894da457aded5b4258181e4fc6a6
Manish Srivastava <mannu927@yahoo.com>
2020-05-12 12:15:00 +0530

    Update LC1114.cpp

--- LC1114.cpp
+++ LC1114.cpp
@@ -10,8 +10,6 @@ public class Foo {
 }
 The same instance of Foo will be passed to three different threads. Thread A will call first(), thread B will call second(), and thread C will call third(). Design a mechanism and modify the program to ensure that second() is executed after first(), and third() is executed after second().
 
- 
-
 Example 1:
 
 Input: [1,2,3]
