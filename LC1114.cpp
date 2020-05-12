6d132ce8ed9038515622afecc362e90d2a60a82d
Manish Srivastava <mannu927@yahoo.com>
2020-05-12 12:18:45 +0530

    Update LC1114.cpp

--- LC1114.cpp
+++ LC1114.cpp
@@ -8,26 +8,32 @@ public class Foo {
   public void second() { print("second"); }
   public void third() { print("third"); }
 }
-The same instance of Foo will be passed to three different threads. Thread A will call first(), thread B will call second(), and thread C will call third(). Design a mechanism and modify the program to ensure that second() is executed after first(), and third() is executed after second().
+The same instance of Foo will be passed to three different threads. Thread A will call first(), 
+thread B will call second(), and thread C will call third(). 
+Design a mechanism and modify the program to ensure that second() is executed after first(), 
+and third() is executed after second().
 
 Example 1:
 
 Input: [1,2,3]
 Output: "firstsecondthird"
-Explanation: There are three threads being fired asynchronously. The input [1,2,3] means thread A calls first(), thread B calls second(), and thread C calls third(). "firstsecondthird" is the correct output.
+Explanation: There are three threads being fired asynchronously. 
+The input [1,2,3] means thread A calls first(), thread B calls second(), and thread C calls third(). 
+"firstsecondthird" is the correct output.
 Example 2:
 
 Input: [1,3,2]
 Output: "firstsecondthird"
-Explanation: The input [1,3,2] means thread A calls first(), thread B calls third(), and thread C calls second(). "firstsecondthird" is the correct output.
+Explanation: The input [1,3,2] means thread A calls first(), thread B calls third(), and thread C calls second().
+"firstsecondthird" is the correct output.
  
-
 Note:
 
-We do not know how the threads will be scheduled in the operating system, even though the numbers in the input seems to imply the ordering. The input format you see is mainly to ensure our tests' comprehensiveness.
+We do not know how the threads will be scheduled in the operating system, even though the numbers in the
+input seems to imply the ordering. The input format you see is mainly to ensure our tests' comprehensiveness.
 */
 
-//Case1 0f 4: Using Mutex and Condition Variables.
+//Approach 1 0f 4: Using Mutex and Condition Variables.
 class Foo 
 {
     std::condition_variable cv;
@@ -68,7 +74,7 @@ public:
     }
 };
 
-//Case 2 of 4: using Volatile
+//Approach 2 of 4: using Volatile
 
 class Foo {
     volatile int count;
@@ -107,7 +113,7 @@ public:
     }
 };
 
-//Case 3 0f 4: Using `semaphone`
+//Approach 3 0f 4: Using `semaphone`
 #include <semaphore.h>
 class Foo {
     sem_t second;
@@ -139,7 +145,7 @@ public:
     }
 };
 
-//Case 4 0f 4: Using `atomic`
+//Approach 4 0f 4: Using `atomic`
 class Foo {
     std::atomic<int> count;
     
